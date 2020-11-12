124b3587bdcd14324e616210609305b45c1aa93d
jjrohrer <jj.bitbucket@rohrer.org>
2020-11-12 14:55:44 -0500

    got workingish

--- resources/views/livewire/user-value-with-shallow-modal.blade.php
+++ resources/views/livewire/user-value-with-shallow-modal.blade.php
@@ -1,32 +1,86 @@
 <div>
-{{--{!! $value !!}--}}
-    <div wire:click="$toggle('showingModal')" wire:loading.attr="disabled">
+    {{--{!! $value !!}--}}
+    <div wire:click="showModal()" wire:loading.attr="disabled">
         <x-tassy::ui.looks.link-to-modal href="tbd">{!! $maybeHighlightedValue !!}</x-tassy::ui.looks.link-to-modal>
     </div>
 
 
-    <!-- Delete User Confirmation Modal -->
+    <!-- Modal -->
     <x-tassy::dialog-modal wire:model="showingModal">
 
-        <x-slot name="title">User: {{$value}}</x-slot>
-        <x-slot name="content">
 
-            <div class="">
-                <x-tassy::ui.looks.label-value label="{{$valueLabel}}" value="{{$value}}"/>
-                <br>
-                <x-tassy::ui.looks.label-value label="Email" value="{{$asrRow['email']}}"/>
-            </div>
+        <x-slot name="title">User: {{$user->name}}</x-slot>
+        <x-slot name="content">
+            <livewire:flash-container key="{{uniqid()}}"/>
+
+            @if ($isInState == 'reading')
+                <div class="">
+                    <x-tassy::ui.looks.label-value label="Name" value="{{$user->name}}"/>
+                    <br>
+                    <x-tassy::ui.looks.label-value label="Email" value="{{$user->email}}"/>
+                </div>
+                <div class="text-right pr-3">
+                    <x-tassy::ui.looks.link-to wire:click.prevent="moveToState('editing')" href='/'>
+                        Edit
+                    </x-tassy::ui.looks.link-to>
+                </div>
+            @elseif ($isInState == 'editing')
+                <x-tassy::ui.looks.link-to href="/" wire:click.prevent="moveToState('reading')" enum-sandbox="back">
+                    Back
+                </x-tassy::ui.looks.link-to>
+                <div class="sm:border-t sm:border-gray-200">
+                   <div class="sm:grid sm:grid-cols-3 sm:gap-3 sm:items-start  sm:pt-3">
+                      <label for="name" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+                        Name
+                      </label>
+                      <div class="mt-1 sm:mt-0 sm:col-span-2">
+                        <div class="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+                          <input id="name" type="text" wire:model="user.name" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                        </div>
+                      </div>
+
+                       @error('user.name') <div class="spacer"></div> <x-tassy::ui.looks.input-error class=" sm:col-span-2  sm:max-w-xs">{{ $message }}</x-tassy::ui.looks.input-error> @enderror
+
+                    </div>
+
+
+                    <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5 ">
+                      <label for="email" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+                        Email
+                      </label>
+                      <div class="mt-1 sm:mt-0 sm:col-span-2">
+                        <div class="max-w-lg rounded-md shadow-sm sm:max-w-xs">
+                          <input id="email" type="text" wire:model="user.email"  class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">
+                        </div>
+                      </div>
+                          @error('user.email') <div class="spacer"></div> <x-tassy::ui.looks.input-error class=" sm:col-span-2  sm:max-w-xs">{{ $message }}</x-tassy::ui.looks.input-error> @enderror
+                    </div>
+
+                </div>
+
+            @else
+                {{assert(0,__FILE__.__LINE__)}}
+            @endif
 
         </x-slot>
         <x-slot name="footer">
-{{--            <span class="text-gray-400">Your footer text goes here</span>--}}
-            <x-jet-secondary-button wire:click="$toggle('showingModal')" wire:loading.attr="disabled">
+             @if ($isInState == 'reading')
+            <x-jet-secondary-button wire:click="closeModal()" wire:loading.attr="disabled">
                 Close
             </x-jet-secondary-button>
-
-{{--            <x-jet-button class="ml-2" wire:click="doModalPrimary" wire:loading.attr="disabled">--}}
-{{--                Click it! Click it!--}}
-{{--            </x-jet-button>--}}
+             @elseif ($isInState == 'editing')
+                 <x-jet-secondary-button wire:click="closeModal()" wire:loading.attr="disabled">
+                                Cancel
+                            </x-jet-secondary-button>
+                            <x-jet-secondary-button wire:click="update()" wire:loading.attr="disabled" class="{{ $canUpdate ?'' : 'text-blue-200'}}">
+                                Update
+                            </x-jet-secondary-button>
+ @else
+                {{assert(0,__FILE__.__LINE__)}}
+            @endif
+            {{--            <x-jet-button class="ml-2" wire:click="doModalPrimary" wire:loading.attr="disabled">--}}
+            {{--                Click it! Click it!--}}
+            {{--            </x-jet-button>--}}
         </x-slot>
 
     </x-tassy::dialog-modal>
--- src/Http/Livewire/UserValueWithShallowModal.php
+++ src/Http/Livewire/UserValueWithShallowModal.php
@@ -6,31 +6,72 @@ use Livewire\Component;
 
 class UserValueWithShallowModal extends Component
 {
-    public string $value;
-    public string $valueLabel = 'Name';
+    public \App\Models\User $user;
+    protected $rules = [
+        'user.name' =>  'required|string|min:2|max:256',
+        'user.email' => 'required|string|max:512|email',
+    ];
     public int $db_id;
     public string $maybeHighlightedValue; // might be html with search term highlighted
-    public array $asrRow;
 
+    private array $enumStates = ['editing','reading'];
+    public string $isInState = 'reading';
     public bool $showingModal = false;
+    public bool $canUpdate = true;
+
 
     public function mount(string $value, int $id, string $maybeHighlightedValue = 'dub', $asrRow)
     {
-        $this->value = $value;
+        #$this->value = $value;
         $this->db_id = $id;
         $this->maybeHighlightedValue = $maybeHighlightedValue;
-        $this->asrRow = $asrRow;
-
+        #$this->asrRow = $asrRow;
+        $this->user = \App\Models\User::find($this->db_id);
+        assert($this->user,__FILE__.__LINE__);
     }
 
-    public function doModalPrimary()
+
+    public function showModal(\MattLibera\LivewireFlash\Livewire\FlashMessage $f) {
+        $this->showingModal = true;
+        $this->isInState = 'reading';
+        session()->forget('flash_notification');
+    }
+
+    public function closeModal() {
+        $this->showingModal = false;
+    }
+
+
+    public function moveToState(string $enumNewState) {
+        assert(in_array($enumNewState, $this->enumStates),__FILE__.__LINE__);
+        assert($enumNewState != $this->isInState,__FILE__.__LINE__);
+        $this->isInState = $enumNewState;
+    }
+
+    public function updated($propertyName)
     {
-        // Do something $this->count = $this->count + 5;
-
-        // Close dialog
-        $this->showingModal = false; // FYI: This is wire's way of closing the modal, since
+        $this->canUpdate = false;
+        $b = $this->validateOnly($propertyName); // validate on change, but
+        $this->canUpdate = true; // Execution doesn't reach here if validation fails.
     }
 
+    public function update()
+    {
+        $validatedData = $this->validate();
+        // Execution doesn't reach here if validation fails.
+
+        $flight = \App\Models\User::find($this->db_id);
+        $flight->name = $validatedData['user']['name'];
+        $flight->email = $validatedData['user']['email'];
+        $b = false;
+        $b = $flight->save();
+        if (!$b) {
+            flash('Ouch: Nothing was saved for '.$flight->getOriginal('name') )->error()->livewire($this);
+        } else {
+            $this->isInState = 'reading';
+            flash('Successfully Updated '.$this->user->name)->success()->livewire($this);
+        }
+    }
 
     public function render()
     {
